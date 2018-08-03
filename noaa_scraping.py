401063a6bf4644046c77c1cb832a2e5572495334
Peter Sanders <peter@psanders.me>
2018-08-02 21:53:21 -0400

    Dependency injection initialization
    Fix initialization of the updated `StationGlobe` object from the previous commit.
    Also I've moved the work into a method to eliminate import side effects.

--- noaa_scraping.py
+++ noaa_scraping.py
@@ -7,6 +7,10 @@ from tinydb import TinyDB, Query
 from geopy import geocoders
 from geopy.extra.rate_limiter import RateLimiter
 
-geolocator = geocoders.Nominatim(user_agent='scubot', timeout=5)
-geolocator.geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
-station_globe = noaa.StationGlobe.scrape_noaa(geolocator, Query())
+def main():
+    geolocator = geocoders.Nominatim(user_agent='scubot', timeout=5)
+    geolocator.geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+    station_globe = noaa.StationGlobe.scrape_noaa(geolocator, TinyDB('noaa.json'), Query())
+
+if __name__ == '__main__':
+    main()
