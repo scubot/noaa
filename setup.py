a697b4bcad1b4888b026ea8365661c0b7a8d12f3
hxtk <peter@psanders.me>
2019-05-07 16:48:36 -0400

    Add setup file

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scubot-tides",
+    version="1.0.0.dev",
+    author="Scubot Team",
+    description="Tide Chart for Scubot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scubot/noaa",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "tinydb",
+        "discord"
+    ],
+    dependency_links=[
+        "https://github.com/scubot/reaction-scroll/tarball/package#egg=reaction-scroll-0.0.1.dev"
+        "https://github.com/hxtk/noaa_py/tarball/master#egg=noaa_py-0.0.1.dev"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
