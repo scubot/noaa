ecc09079fc6e862dee1fb42fb4c954aeff6b5fe3
hxtk <peter@psanders.me>
2019-10-18 19:39:49 -0400

    Update reactionscroll to use prototype

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
         "discord",
 
         "reaction-scroll @ git+https://github.com/scubot/reaction-scroll.git"
-        "@e99ad003bc9a96f6f287f9ec09c551abbb4ccefd",
+        "@47d3f6c2a4c6c669daf48573343ebc5fca7c9b18",
 
         "noaa_py @ git+https://github.com/hxtk/noaa_py.git"
         "@54b3eb3471e72aad54d83d147feb1ff89442d43c"
