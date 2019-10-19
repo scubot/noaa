05f14f18193b46d1827de03c98eff24ded3d23d0
hxtk <peter@psanders.me>
2019-10-18 20:05:09 -0400

    Update dependency version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
         "discord",
 
         "reaction-scroll @ git+https://github.com/scubot/reaction-scroll.git"
-        "@47d3f6c2a4c6c669daf48573343ebc5fca7c9b18",
+        "@996c4552ad07fe7d0cdc349548eec8051297298e",
 
         "noaa_py @ git+https://github.com/hxtk/noaa_py.git"
         "@54b3eb3471e72aad54d83d147feb1ff89442d43c"
