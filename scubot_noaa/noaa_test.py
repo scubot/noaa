0538b4a97da1b0eb290d2a67fd4fb1ac8a9d30d8
hxtk <peter@psanders.me>
2019-10-20 11:43:14 -0400

    Add first test

---
+++ scubot_noaa/noaa_test.py
@@ -0,0 +1,18 @@
+# pylint: disable C0103
+import datetime
+
+import pytest
+from noaa import tides
+
+from . import noaa
+
+
+class TestNoaa:
+    def test_split_data(self):
+        today = datetime.datetime.now()
+        yesterday = today - datetime.timedelta(days=1)
+        tide_1 = tides.PredictionsRow(today, 0.0, 'L'),
+        tide_2 = tides.PredictionsRow(yesterday, 0.0, 'L')
+        data = [tide_1, tide_2]
+        output = split_data(data)
+        assert output == [[tide_1], [tide_2]]
